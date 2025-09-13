09a44622243adc784791677d85a0f23bfc4c1620
SrilekhaS20 <lekhaconst.1@gmail.com>
2025-09-13 10:27:48 +0530

    Started shell scripting with variables

---
+++ Day-25/shell-scripting/variables/sample.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Below are some examples of predefined or system variables in bash
+echo $BASH # Displays the path of the bash shell
+echo $HOME # Displays the home directory of the current user
+echo $PWD # Displays the present working directory
+echo $BASH_VERSION # Displays the version of the bash shell
+
+# Below is an example of a user-defined variable in bash
+name=Sri
+echo "Hello, $name" # Displays Hello, Sri
