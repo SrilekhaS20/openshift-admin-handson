b193214f0401719a6f43e4540a6f4a2b4b822df5
SrilekhaS20 <lekhaconst.1@gmail.com>
2025-09-15 20:18:52 +0530

    Append text to end of text file

---
+++ Day-25/shell-scripting/6.append_text_to_file/append.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "Enter the name of the file: \c"
+read file_name
+
+if [ -f $file_name ]
+then
+  if [ -w $file_name ]
+  then
+    echo "Type some text. To quit, press Ctrl+D."
+    cat >> $file_name
+  else
+    echo "$file_name does not have write permission."
+  fi
+else
+  echo "$file_name does not exist."
+fi
